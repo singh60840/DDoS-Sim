684a3d7e26bd6450ef6bc4d719d1cc9d9ff280cd
Amardeep Singh <125386081+singh60840@users.noreply.github.com>
2024-09-23 05:32:15 +0000

    added run.sh script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+pip install flask psutil aiohttp
+
+if sudo lsof -t -i:8080 > /dev/null; then
+    echo "Killing process on port 8080..."
+    sudo kill -9 $(sudo lsof -t -i:8080)
+else
+    echo "No process running on port 8080."
+fi
+
+python ddos-lite.py
